bd0a34878d540ebb29fa2bcdc816b317909b7ba9
suberlak <suberlak@uw.edu>
2022-05-02 11:52:30 -0700

    run autopep8 on javelin/gp/Mean.py

--- javelin/gp/Mean.py
+++ javelin/gp/Mean.py
@@ -1,11 +1,12 @@
 # Copyright (c) Anand Patil, 2007
 
-__docformat__='reStructuredText'
-
-from numpy import *
 from .GPutils import regularize_array, trisolve
+from numpy import *
+__docformat__ = 'reStructuredText'
 
-__all__ = ['Mean','zero_fn']
+
+__all__ = ['Mean', 'zero_fn']
+
 
 class Mean(object):
     """
@@ -16,7 +17,7 @@ class Mean(object):
 
 
     :Arguments:
-    
+
         -   `eval_fun`: A function that takes an argument x of shape  (n,ndim), where n is
             any integer and ndim is the dimensionality of  the space, or shape (n). In the
             latter case ndim should be assumed to be 1..
@@ -44,7 +45,7 @@ class Mean(object):
         self.eval_fun = eval_fun
         self.params = params
 
-    def observe(self, C, obs_mesh_new, obs_vals_new, mean_under = None):
+    def observe(self, C, obs_mesh_new, obs_vals_new, mean_under=None):
         """
         Synchronizes self's observation status with C's.
         Values of observation are given by obs_vals.
@@ -70,10 +71,10 @@ class Mean(object):
 
             self.dev = (obs_vals_new - mean_under_new)
 
-    	    self.reg_mat = C._unobs_reg(self)
+            self.reg_mat = C._unobs_reg(self)
 
         # If self has been observed already:
-        elif len(obs_vals_new)>0:
+        elif len(obs_vals_new) > 0:
 
             # Rank of old observations.
             m_old = len(self.dev)
@@ -81,41 +82,41 @@ class Mean(object):
             # Deviation of new observation from mean without regard to old observations.
             dev_new = (obs_vals_new - mean_under_new)
 
-
             # Again, basis covariances get special treatment.
-    	    self.reg_mat = C._obs_reg(self, dev_new, m_old)
+            self.reg_mat = C._obs_reg(self, dev_new, m_old)
 
             # Stack deviations of old and new observations from unobserved mean.
             self.dev = hstack((self.dev, dev_new))
         self.observed = True
 
-    def __call__(self, x, observed = True, regularize=True, Uo_Cxo=None):
+    def __call__(self, x, observed=True, regularize=True, Uo_Cxo=None):
 
         # Record original shape of x and regularize it.
         orig_shape = shape(x)
-        if len(orig_shape)>1:
+        if len(orig_shape) > 1:
             orig_shape = orig_shape[:-1]
 
         if regularize:
-            x=regularize_array(x)
+            x = regularize_array(x)
 
         ndimx = x.shape[-1]
         lenx = x.shape[0]
 
-
         # Safety.
         if self.ndim is not None:
             if not self.ndim == ndimx:
-                raise ValueError("The number of spatial dimensions of x does not match the number of spatial dimensions of the Mean instance's base mesh.")
+                raise ValueError(
+                    "The number of spatial dimensions of x does not match the number of spatial dimensions of the Mean instance's base mesh.")
 
         # Evaluate the unobserved mean
-        M = self.eval_fun(x,**self.params).squeeze()
+        M = self.eval_fun(x, **self.params).squeeze()
 
         # Condition. Basis covariances get special treatment. See documentation for formulas.
         if self.observed and observed:
             M = self.C._obs_eval(self, M, x, Uo_Cxo)
 
         return M.reshape(orig_shape)
+
 
 def zero_fn(x):
     "A function mapping any argument to zero."
